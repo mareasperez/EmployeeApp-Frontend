e9b5cbd0eb408890a9d99ebecbbcfb29d33bf69d
Maury Areas <mareasperez7@gmail.com>
2021-08-09 00:28:25 -0600

    initial app schema: components: -Departament -Employee -Home

---
+++ src/app/components/employeee/employeee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employeee',
+  templateUrl: './employeee.component.html',
+  styleUrls: ['./employeee.component.css']
+})
+export class EmployeeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
